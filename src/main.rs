c96aa7a85d3ffaf9b090a3aba2d9dde4611163b2
chris <kakaxi77749@gmail.com>
2022-06-30 15:33:18 +0800

    basic test

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,14 @@ use rich_strong_lib::{server::Server, head::LineType, config::Config, log::Log};
 fn main() {
     Log::create_log_dir();
     Config::init();
-    let (app_addr,_) = Config::get_listen_addr();
-    
+    let (app_addr,http_addr) = Config::get_listen_addr();
     let mut app = Server::new(app_addr,LineType::Fox);
+    let mut http = Server::new(http_addr,LineType::Http);
+    
+    std::thread::spawn(move ||{
+        http.start();
+    });
+
+    app.init();
     app.start();
 }
