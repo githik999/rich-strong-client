c97c6e234ade0c3e8c870c2702ca14ba75c996b5
chris <kakaxi77749@gmail.com>
2022-06-30 17:53:25 +0800

    pub

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ use rich_strong_lib::{server::Server, head::LineType, config::Config, log::Log};
 
 fn main() {
     Log::create_log_dir();
-    Config::init();
-    let (app_addr,http_addr) = Config::get_listen_addr();
+    
+    let (app_addr,http_addr) = Config::init();
+    Config::init_client_side_setting();
+    
     let mut app = Server::new(app_addr,LineType::Fox);
     let mut http = Server::new(http_addr,LineType::Http);
     
