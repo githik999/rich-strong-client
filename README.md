3f12df466caeb1da1045fbc28850177239e16f1c
ClearLove7 <kakaxi77749@gmail.com>
2022-07-20 16:00:12 +0800

    remove http

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rich-strong-client
 the client side of rich strong (a socks5 proxy written in Rust)
+
+# common errors
+1.can not connect to proxy server
--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,11 @@ use rich_strong_lib::{server::Server, head::LineType, config::Config, log::Log};
 fn main() {
     Log::create_log_dir();
     
-    let (app_addr,http_addr) = Config::init();
+    let app_addr = Config::init();
     Config::init_client_side_setting();
     
     let mut app = Server::new(app_addr,LineType::Fox);
-    let mut http = Server::new(http_addr,LineType::Http);
     
-    std::thread::spawn(move ||{
-        http.start();
-    });
-
     app.init();
     app.start();
 }
